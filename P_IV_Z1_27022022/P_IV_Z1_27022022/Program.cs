1bc30cbb58a6680fdbd874e0b7c14b6dad91d144
Dawid Foryś <dawidfor4@gmail.com>
2022-02-27 12:12:52 +0100

    Init

---
+++ P_IV_Z1_27022022/P_IV_Z1_27022022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P_IV_Z1_27022022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
